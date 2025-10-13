2246a6feac98e609dda80fc7b83c4c4e111ac9b4
Jan Britz <britz@tu-berlin.de>
2025-10-13 12:40:33 +0200

    chore: set Moodle 5.0 as a minimum

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'assignsubmission_qpy';
 $plugin->version = 2025092500;
-$plugin->requires = 2024100700;
+$plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1';
 
